be5422c0bf4fd1f2d40f5eeb9c7c6e80e5cfb55f
Rogue6X <assassing13@gmail.com>
2021-06-02 20:53:24 +0300

    Finished LinQ to XML queries

---
+++ lab5/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace lab5
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Queries.CreateXMLfromTable(Queries.people, "users", Queries.devices, "devices");
+            Queries.SimpleQ();
+        }
+    }
+}
---
+++ lab5/User.cs
@@ -0,0 +1,18 @@
+﻿namespace lab5
+{
+    public class User
+    {
+        public string name { get; }
+        public string surname { get; }
+
+        public User(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+        public override string ToString()
+        {
+            return name+" "+surname;
+        }
+    }
+}
